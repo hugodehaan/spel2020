61c10c57cb4669ace3ef0d9d6ab16c90d4cecf92
Hugo de Haan <110176@st-maartenscollege.nl>
2020-07-02 15:24:09 +0000

    fail fixed + guess added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 def spel() :
+
   # de woorden die gebruikt gaan worden
   woordenlijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier",
   "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
@@ -10,3 +11,19 @@ def spel() :
   puntjes = [" ~ "] *lengtewoord
   ja = "ja"
   nee = "nee"
+
+  # introductie
+  naam = input("Vul hier je naam in: ")
+  print()
+  print("Welkom", naam,"bij ons galgje spel")
+  game = True
+
+  print("Raad de letters en als je het woord weet typ het dan in.")
+  print()
+  print("het woord heeft " + str(lengtewoord) + " letters")
+
+  while game == True:
+    userGuess = input("geef letter/woord> ")
+    # zit letter in woord? Zo ja, waar dan?
+
+spel()
