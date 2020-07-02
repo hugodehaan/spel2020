a05071f5c0e6c23cf439eaa5526e6a0099bfecd0
Hugo de Haan <110176@st-maartenscollege.nl>
2020-07-02 17:26:59 +0000

    tekening galgje plus levens toegevoegd en verliestekst

--- main.py
+++ main.py
@@ -1,6 +1,101 @@
 import random
 def spel() :
+
   turns = 10
+  
+  def drawgalgjes():
+    if turns == 9:
+        print("Jammer, nog 8 pogingen")
+        print("""  
+         |
+         |
+         |
+         |
+         |
+    _____|""")
+
+    if turns == 8:
+        print("Jammer, nog 7 pogingen te gaan")
+        print("""     ____
+         |
+         |
+         |
+         |
+         |
+    _____|""")
+
+    if turns == 7:
+        print("Jammer, nog 6 pogingen te gaan")
+        print("""     ____
+        \|
+         |
+         |
+         |
+         |
+    _____|""")
+
+    if turns == 6:
+        print("Jammer, nog 5 pogingen te gaan")
+        print("""     ____
+      | \|
+         |
+         |
+         |
+         |
+    _____|""")
+
+    if turns == 5:
+        print("Jammer, nog 4 pogingen te gaan")
+        print("""     ____
+      | \|
+      o  |
+         |
+         |
+         |
+    _____|""")
+
+    if turns == 4:
+        print("Jammer, nog 3 pogingen te gaan")
+        print("""     ____
+      | \|
+      o  |
+      |  |
+         |
+         |
+    _____|""")
+
+    if turns == 3:
+        print("Jammer, nog 2 poging te gaan")
+        print("""     ____
+      | \|
+      o  |
+     /|\ |
+         |
+         |
+    _____|""")
+
+    if turns == 2:
+        print("Pas op!!!, nog 1 poging te gaan")
+        print("""     ____
+      | \|
+      o  |
+     /|\ |
+     / \ |
+         |
+    _____|""")
+
+    if turns == 1:
+        print("""     ____
+      | \|
+       o |
+     /|\ |
+     / \ |
+         |
+    _____|""")
+        print("Schande!, Je hebt het woord", hetwoord, "niet kunnen raden", naam, "probeer het nog een keer!")
+        print()
+        spel();
+    
 
   # de woorden die gebruikt gaan worden
   woordenlijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier",
