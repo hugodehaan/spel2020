bdfc0d9726c7752b13c50ab28efc1f4bdd94b39e
Hugo de Haan <110176@st-maartenscollege.nl>
2020-07-02 15:27:36 +0000

    added valid letters

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def spel() :
   # de woorden die gebruikt gaan worden
   woordenlijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier",
   "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
+  
+  # letters die zijn toegestaan
+  validLetters = "a", "b", "c", "d", "", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+   
   # random woord word gekozen en door puntjes vervangen 
 
   hetwoord= random.choice(woordenlijst)
